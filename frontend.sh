d63f4392ea61491736bc9971b0b93fe079fe8529
ASatheesh <satheeshovsn@gmail.com>
2023-11-03 21:41:04 +0530

    frontend-commit

--- frontend.sh
+++ frontend.sh
@@ -1,27 +1,27 @@
 source common.sh
 
 echo  -e "\e[36m>>>>>>>>>>>>>>>>>>install nginx <<<<<<<<<<<<<\e[0m" | tee  -a ${log}
-yum install nginx -y
+yum install nginx -y &>>${log}
 func_exit_status
 
 echo  -e "\e[36m>>>>>>>>>>>>>>>>>>copy roboshop configuration <<<<<<<<<<<<<\e[0m" | tee  -a ${log}
-cp nginx-roboshop.conf  /etc/nginx/default.d/roboshop.conf
+cp nginx-roboshop.conf  /etc/nginx/default.d/roboshop.conf &>>${log}
 func_exit_status
 
 echo  -e "\e[36m>>>>>>>>>>>>>>>>>>remove old content <<<<<<<<<<<<<\e[0m" | tee  -a ${log}
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/* &>>${log}
 func_exit_status
 
 echo  -e "\e[36m>>>>>>>>>>>>>>>>>>download application content <<<<<<<<<<<<<\e[0m" | tee  -a ${log}
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log}
 func_exit_status
 
-cd /usr/share/nginx/html
+cd /usr/share/nginx/html &>>${log}
 echo  -e "\e[36m>>>>>>>>>>>>>>>>>>extract application content <<<<<<<<<<<<<\e[0m" | tee  -a ${log}
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip &>>${log}
 func_exit_status
 
 echo  -e "\e[36m>>>>>>>>>>>>>>>>>>start nginx <<<<<<<<<<<<<\e[0m" | tee  -a ${log}
-systemctl enable nginx
-systemctl restart nginx
+systemctl enable nginx &>>${log}
+systemctl restart nginx &>>${log}
 func_exit_status
