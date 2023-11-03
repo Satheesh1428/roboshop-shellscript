2c89ae4861480a7fe2f64d926da77c510bd199a8
ASatheesh <satheeshovsn@gmail.com>
2023-11-03 23:16:15 +0530

    frontend-commit

--- mysql.sh
+++ mysql.sh
@@ -1,3 +1,9 @@
+mysql_root_password = $1
+if [ -z "${mysql_root_password}"  ]; then
+    echo  -e "\e[36m input password is missing \e[0m"
+    exit 1
+fi
+
 cp mysql.repo  /etc/yum.repos.d/mysql.repo
 
 yum module disable mysql -y
@@ -7,4 +13,5 @@ yum install mysql-community-server -y
 systemctl enable mysqld
 systemctl restart mysqld
 
-mysql_secure_installation --set-root-pass RoboShop@1
+#mysql_secure_installation --set-root-pass RoboShop@1
+mysql_secure_installation --set-root-pass ${mysql_root_password}
